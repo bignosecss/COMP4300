e6ca332bebd066cddb577594044e0656739569cf
bignosecss <bignosecss@gmail.com>
2024-01-16 12:10:43 +0800

    set the text to the center of a rectangle.

--- Assignment1/src/main.cpp
+++ Assignment1/src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <filesystem>
-#include <fstream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
@@ -10,28 +9,19 @@ int main(int argc, char* argv[]) {
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "My SFML");
 
+    // Rectangle
+    sf::Vector2f rSize(250.0f, 100.0f);
+    sf::RectangleShape rect(rSize);
+    sf::Vector2f rectMoveSpeed(0.1f, 0.05f);
+    rect.setFillColor(sf::Color(255, 215, 0));
+
     // 字体
     sf::Font font;
-    if (!font.loadFromFile("bin/fonts/Wizard-2Omwv.otf")) {
+    if (!font.loadFromFile("bin/fonts/coffee-shop-bold-small-cap.ttf")) {
         return -1;
     }
     sf::Text text("Hello World!", font, 24);
     text.setFillColor(sf::Color::Red);
-    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-    text.setPosition(420.f, 200.f);
-    text.setRotation(45.f);
-    text.rotate(10.f);
-    float rotation = text.getRotation();
-    text.scale(1.5f, 2.0f);
-    std::cout << "The text rotate: " << rotation << std::endl;
-
-    // texture
-    sf::Texture texture;
-    if (!texture.loadFromFile("bin/images/Jordan.jpg", sf::IntRect(10, 10, 320, 320))) {
-        return -1;
-    }
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -39,10 +29,25 @@ int main(int argc, char* argv[]) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            if (event.type == sf::Event::KeyPressed) {
+                std::cout << "Key pressed with code = " << event.key.code << std::endl;
+                if (event.key.code == sf::Keyboard::X) {
+                    rectMoveSpeed *= -1.0f;
+                }
+            }
         }
+
+        rect.setPosition(rect.getPosition().x + rectMoveSpeed.x, rect.getPosition().y + rectMoveSpeed.y);
+
+        sf::Vector2f centerRect(rect.getPosition().x + rect.getSize().x / 2,
+                                rect.getPosition().y + rect.getSize().y / 2);
+
+        text.setPosition(centerRect.x - text.getGlobalBounds().width / 2, 
+                         centerRect.y - text.getGlobalBounds().height / 2);
+
         window.clear();
+        window.draw(rect);
         window.draw(text);
-        window.draw(sprite);
         window.display();
     }
 
