c7050aae7fe35922d08f79f29c998e27bedd1a74
bignosecss <bignosecss@gmail.com>
2024-01-17 23:37:57 +0800

    pass all shape test.

--- Assignment1/src/main.cpp
+++ Assignment1/src/main.cpp
@@ -9,50 +9,21 @@ public:
     sf::RectangleShape   m_rect;
     sf::CircleShape      m_circle;
     sf::Vector2f         m_speed;
+    std::string          shapeName;
 
     virtual void setPosition(const sf::Vector2f& position) = 0;
     virtual void setMoveSpeed(const sf::Vector2f& speed)  = 0;
     virtual void update(const sf::RenderWindow& window) = 0;
     virtual void draw(sf::RenderWindow& window) = 0;
-
-    void loadFromFile(const std::string& filename) {
-        std::ifstream fin(filename);
-
-        std::string settingType;
-        // components
-        float       xxPos, yyPos;
-        float       xxVel, yyVel;
-        int         R, G, B;
-        // rectangle
-        float       rectWidth, rectHeight;
-        // circle
-        float       radius;
-
-        while (fin >> settingType) {
-            if (settingType == "Rectangle") {
-                fin >> xxPos >> yyPos;
-                fin >> xxVel >> yyVel;
-                fin >> R >> G >> B;
-                fin >> rectWidth >> rectHeight;
-
-                std::cout << xxPos << " " << yyPos << " " << xxVel << " " << yyVel << " ";
-                std::cout << R << " " << G << " " << B << " ";
-                std::cout << rectWidth << " " << rectHeight << std::endl;
-            } else if (settingType == "Circle") {
-                fin >> xxPos >> yyPos;
-                fin >> xxVel >> yyVel;
-                fin >> R >> G >> B;
-                fin >> radius;
-
-                std::cout << xxPos <<  " " << yyPos <<  " " << xxVel <<  " " << yyVel << " ";
-                std::cout << R <<  " " << G <<  " " << B <<  " " << radius << std::endl;
-            }
-        }
-    }
+    virtual void loadFromFile(const std::string& filename) = 0;
 };
 
 class MyRectangle : public MyShape {
 public:
+    std::vector<std::shared_ptr<MyRectangle>> m_rects;
+
+    MyRectangle() {}
+
     MyRectangle(const sf::Vector2f& size, int R, int G, int B)
         : MyShape() {
         m_rect.setSize(size);
@@ -85,11 +56,47 @@ public:
     void draw(sf::RenderWindow& window) override {
         window.draw(m_rect);
     }
+
+    void loadFromFile(const std::string& filename) override {
+        std::ifstream fin(filename);
+
+        std::string settingType;
+        // rectangle
+        std::string shapeName;
+        float       xxPos, yyPos;
+        float       xxVel, yyVel;
+        int         R, G, B;
+        float       rectWidth, rectHeight;
+
+        while (fin >> settingType) {
+            if (settingType == "Rectangle") {
+                fin >> shapeName;
+                fin >> xxPos >> yyPos;
+                fin >> xxVel >> yyVel;
+                fin >> R >> G >> B;
+                fin >> rectWidth >> rectHeight;
+
+                // store the rectangles
+                std::shared_ptr<MyRectangle> rect = std::make_shared<MyRectangle>(sf::Vector2f(rectWidth, rectHeight), R, G, B);
+                rect->setPosition(sf::Vector2f(xxPos, yyPos));
+                rect->setMoveSpeed(sf::Vector2f(xxVel, yyVel));
+                m_rects.push_back(rect);
+                // print all info
+                std::cout << shapeName << " ";
+                std::cout << xxPos << " " << yyPos << " " << xxVel << " " << yyVel << " ";
+                std::cout << R << " " << G << " " << B << " ";
+                std::cout << rectWidth << " " << rectHeight << std::endl;
+            }
+        }
+    }
 };
 
 class MyCircle : public MyShape {
 public:
-    sf::Vector2f m_circleMoveSpeed;
+    sf::Vector2f                           m_circleMoveSpeed;
+    std::vector<std::shared_ptr<MyCircle>> m_circles;
+
+    MyCircle() {}
 
     MyCircle(float r, int R, int G, int B)
         : MyShape() {
@@ -122,39 +129,52 @@ public:
     void draw(sf::RenderWindow& window) override {
         window.draw(m_circle);
     }
+
+    void loadFromFile(const std::string& filename) override {
+        std::ifstream fin(filename);
+
+        std::string settingType;
+        // rectangle
+        std::string shapeName;
+        float       xxPos, yyPos;
+        float       xxVel, yyVel;
+        int         R, G, B;
+        float       radius;
+
+        while (fin >> settingType) {
+            if (settingType == "Circle") {
+                fin >> shapeName;
+                fin >> xxPos >> yyPos;
+                fin >> xxVel >> yyVel;
+                fin >> R >> G >> B;
+                fin >> radius;
+
+                // store the circles
+                std::shared_ptr<MyCircle> circle = std::make_shared<MyCircle>(radius, R, G, B);
+                circle->setPosition(sf::Vector2f(xxPos, yyPos));
+                circle->setMoveSpeed(sf::Vector2f(xxVel, yyVel));
+                m_circles.push_back(circle);
+                // print all info
+                std::cout << shapeName << " ";
+                std::cout << xxPos <<  " " << yyPos <<  " " << xxVel <<  " " << yyVel << " ";
+                std::cout << R <<  " " << G <<  " " << B <<  " " << radius << std::endl;
+            }
+        }
+    }
 };
 
 int main(int argc, char* argv[]) {
 
     std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
 
-    const int wWidth = 1280;
-    const int wHeight = 720;
+    const int wWidth = 800;
+    const int wHeight = 600;
     sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "My SFML");
 
-    // two rectangles
-    std::shared_ptr<MyRectangle> myRect1 = std::make_shared<MyRectangle>(sf::Vector2f(100, 60), 144, 238, 144);
-    myRect1->setPosition(sf::Vector2f(100, 60));
-    myRect1->setMoveSpeed(sf::Vector2f(0.1f, 0.05f));
-    std::shared_ptr<MyRectangle> myRect2 = std::make_shared<MyRectangle>(sf::Vector2f(150, 120), 255, 69, 0);
-    myRect2->setPosition(sf::Vector2f(300, 200));
-    myRect2->setMoveSpeed(sf::Vector2f(-0.05f, -0.1f));
-
-    // two circles
-    std::shared_ptr<MyCircle> myCircle1 = std::make_shared<MyCircle>(20.0f, 255, 0, 0);
-    myCircle1->setPosition(sf::Vector2f(200, 200));
-    myCircle1->setMoveSpeed(sf::Vector2f(0.15f, 0.5f));
-    std::shared_ptr<MyCircle> myCircle2 = std::make_shared<MyCircle>(30.0f, 255, 215, 0);
-    myCircle2->setPosition(sf::Vector2f(150, 100));
-    myCircle2->setMoveSpeed(sf::Vector2f(-0.5f, -0.15f));
-
-    myRect1->loadFromFile(argv[1]);
-
-    std::vector<std::shared_ptr<MyShape>> shapes;
-    shapes.push_back(myRect1);
-    shapes.push_back(myRect2);
-    shapes.push_back(myCircle1);
-    shapes.push_back(myCircle2);
+    MyRectangle rect;
+    MyCircle    circle;
+    rect.loadFromFile(argv[1]);
+    circle.loadFromFile(argv[1]);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -166,7 +186,10 @@ int main(int argc, char* argv[]) {
                 std::cout << "Key pressed with code = " << event.key.code << std::endl;
                 if (event.key.code == sf::Keyboard::X) {
                     // reverse the direction of speed
-                    for (auto& shape : shapes) {
+                    for (auto& shape : rect.m_rects) {
+                        shape->m_speed *= -1.0f;
+                    }
+                    for (auto& shape : circle.m_circles) {
                         shape->m_speed *= -1.0f;
                     }
                 }
@@ -175,7 +198,11 @@ int main(int argc, char* argv[]) {
 
         window.clear();
         // 更新并画出所有图形
-        for (auto& shape : shapes) {
+        for (auto& shape : rect.m_rects) {
+            shape->update(window);
+            shape->draw(window);
+        }
+        for (auto& shape : circle.m_circles) {
             shape->update(window);
             shape->draw(window);
         }
